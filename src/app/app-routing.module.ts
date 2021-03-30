653ec613cf6114a89f9e37bce28af5169cddd8d5
Pedro <pedrotzf@gmail.com>
2021-03-30 15:26:47 -0300

    define some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './modules/layout/layout.component';
+import { SignUpComponent } from './modules/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LayoutComponent, children: [
+    { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
+    { path: 'sign-up', component: SignUpComponent }
+  ] }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
