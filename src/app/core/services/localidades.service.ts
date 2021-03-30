ad0091f74981401e1f2eaddbb910324ab721147b
Pedro <pedrotzf@gmail.com>
2021-03-30 16:31:09 -0300

    first service to get all states of brazil

--- src/app/core/services/localidades.service.ts
+++ src/app/core/services/localidades.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { UF } from 'src/app/shared/models/uf.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalidadesService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getUfs() {
+    return this.http
+            .get<UF[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+            .pipe(
+              map((estados: UF[]) => estados.map(uf => new UF().deserialize(uf))
+              )
+            )
+  }
 }
