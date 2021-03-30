b2b4582ced068054462045a883ea3cc0158d5411
Pedro <pedro.zulian@nkodontologia.com.br>
2021-03-30 14:35:13 -0300

    needed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './modules/layout/layout.module';
 import { AppComponent } from './app.component';
-import { LayoutComponent } from './modules/layout/layout.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    LayoutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LayoutComponent } from './layout.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
+    ],
   imports: [
     CommonModule
+  ], 
+  exports: [
+    LayoutComponent
   ]
 })
 export class LayoutModule { }
