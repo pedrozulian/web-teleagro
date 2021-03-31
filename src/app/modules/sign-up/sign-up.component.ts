742abbabb7c192945f62f71ee4395d7fe37bda56
Pedro <pedrotzf@gmail.com>
2021-03-30 21:31:33 -0300

    func to sign up call service

--- src/app/modules/sign-up/sign-up.component.ts
+++ src/app/modules/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocalidadesService } from 'src/app/core/services/localidades.service';
+import { UsuariosService } from 'src/app/core/services/usuarios.service';
 import { UF } from 'src/app/shared/models/uf.model';
 
 @Component({
@@ -12,7 +13,8 @@ export class SignUpComponent implements OnInit {
 
   constructor(
     private localidades: LocalidadesService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private usuariosService: UsuariosService
   ) { }
 
   estados: UF[];
@@ -29,11 +31,21 @@ export class SignUpComponent implements OnInit {
         .subscribe(data => this.estados = data);
   }
 
-  cadastrar() {}
+  cadastrar() {
+    if (this.form.valid) {
+      console.log('cadastrar: ', this.form.value);
+      this.usuariosService
+        .cadastrar(this.form.value)
+        .toPromise()
+        .then((data: { resultado: {mensagem: string, nome: string, email: string }}) => { console.log(data.resultado.mensagem) })
+        .catch(error => console.error(error));
+    }
+  }
 
   private criarForm(): void {
     this.form = this.formBuilder.group({
       nome: ['', Validators.required],
+      nome_usuario: ['', Validators.required],
       email: ['', Validators.required],
       senha: ['', Validators.required],
       cidade: ['', Validators.required],
