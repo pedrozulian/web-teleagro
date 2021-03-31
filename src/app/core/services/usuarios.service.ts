f7bf94b2c0b368bf78438e3d6e8a9f5a1db2d5c7
Pedro <pedrotzf@gmail.com>
2021-03-30 21:29:23 -0300

    service with first func to sign up user

---
+++ src/app/core/services/usuarios.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UtilsService } from './utils.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+  constructor(
+    private http: HttpClient,
+    private utils: UtilsService
+  ) { }
+
+  cadastrar(form): Observable<any> {
+    console.log('service: ', form);
+    return this.http
+            .post(`${this.utils.apiUrl}/usuarios/cadastro/`, form);
+  }
+}
