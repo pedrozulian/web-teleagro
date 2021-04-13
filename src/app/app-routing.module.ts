d681899c843ab8e043f05a781efdfb4d1c0d5791
Pedro <pedrotzf@gmail.com>
2021-04-13 20:28:54 -0300

    feat(app-routing.module.ts): new path to signIn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './modules/layout/layout.component';
+import { SignInComponent } from './modules/sign-in/sign-in.component';
 import { SignUpComponent } from './modules/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: LayoutComponent, children: [
     { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
-    { path: 'sign-up', component: SignUpComponent }
+    { path: 'sign-up', component: SignUpComponent },
+    { path: 'sign-in', component: SignInComponent }
   ] }
 ];
 
