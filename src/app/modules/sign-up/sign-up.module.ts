a795919adb5d48d485e95dfc98efdca0905044e7
Pedro <pedrotzf@gmail.com>
2021-04-13 20:03:13 -0300

    feat(sign-up.module.ts): import toasts container

--- src/app/modules/sign-up/sign-up.module.ts
+++ src/app/modules/sign-up/sign-up.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToastsContainer } from '../../shared/components/toasts-global/toasts-container.component';
 
 
 
 @NgModule({
   declarations: [
-    SignUpComponent
+    SignUpComponent,
+    ToastsContainer
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgbModule
   ],
   exports: [
     SignUpComponent
