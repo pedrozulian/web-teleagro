757cde1067e7ba67348e927bb21a871b45145a42
Pedro <pedrotzf@gmail.com>
2021-06-05 12:43:11 -0300

    feat(app.routing): definindo rota do componente profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './modules/layout/layout.component';
+import { ProfileComponent } from './modules/profile/profile.component';
 import { SignInComponent } from './modules/sign-in/sign-in.component';
 import { SignUpComponent } from './modules/sign-up/sign-up.component';
 
@@ -9,7 +10,8 @@ const routes: Routes = [
   { path: '', component: LayoutComponent, children: [
     { path: '', redirectTo: 'sign-up', pathMatch: 'full' },
     { path: 'sign-up', component: SignUpComponent },
-    { path: 'sign-in', component: SignInComponent }
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'profile', component: ProfileComponent }
   ] }
 ];
 
