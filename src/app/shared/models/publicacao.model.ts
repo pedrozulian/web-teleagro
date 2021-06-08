b95d981a5a876f208858ce444f51c6f351c2be02
Pedro <pedrotzf@gmail.com>
2021-06-08 20:47:04 -0300

    feat(publicacao.model): novo atributo

--- src/app/shared/models/publicacao.model.ts
+++ src/app/shared/models/publicacao.model.ts
@@ -4,6 +4,7 @@ export class Publicacao implements Deserializable {
     id_publicacao?: number;
     id_tipo_imagem?: number;
     id_usuario?: number;
+    foto_usuario?: string;
     nome_usuario?: string;
     texto?: string;
     url?: string;
