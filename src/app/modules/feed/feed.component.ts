77bc982c55e398cd29667a8c7c62f8abccf1c341
Pedro <pedrotzf@gmail.com>
2021-06-08 19:53:21 -0300

    feat(feed.component): funcionalidade de salvar publicacao

--- src/app/modules/feed/feed.component.ts
+++ src/app/modules/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { PublicacoesService } from 'src/app/core/services/publicacoes.service';
 import { Publicacao } from 'src/app/shared/models/publicacao.model';
 
@@ -10,20 +11,46 @@ import { Publicacao } from 'src/app/shared/models/publicacao.model';
 export class FeedComponent implements OnInit {
 
   imagemCarregada = false;
+  urlImagem: string;
   publicacoes: Publicacao[];
+  form: FormGroup;
 
   constructor(
-    private publicacoesService: PublicacoesService
+    private publicacoesService: PublicacoesService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.criarForm();
     this.carregarPublicacoes();
   }
 
+  criarForm() {
+    this.form = this.formBuilder.group({
+      texto: ['']
+    });
+  }
+
+  salvarPublicacao() {
+    console.log('aaa');
+    const publi = { id_usuario: 1, id_tipo_imagem: 2, url: this.urlImagem, texto: this.form.get('texto').value };
+    const publicacao = new Publicacao().deserialize(publi);
+    this.publicacoesService
+      .salvarPublicacao(publicacao)
+        .subscribe(() => {
+          this.carregarPublicacoes();
+          this.imagemCarregada = false;
+          this.form.reset();
+        });
+  }
+
   processImage(event) {
-    const file = event.target.files[0]
     this.imagemCarregada = true;
-    this.publicacoesService.salvarImagem(file);
+    this.urlImagem = event.target.files[0].name;
+    // const publicacao = new Publicacao();
+    // publicacao.url = file.name;
+    // publicacao.id_tipo_imagem = 2;
+    // this.publicacoesService.salvarPublicacao(publicacao);
   }
 
   carregarPublicacoes() {
