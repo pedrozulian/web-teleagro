52255b70dd57a88536cc77d0d01537f0d60674c1
Pedro <pedrotzf@gmail.com>
2021-06-08 18:59:07 -0300

    feat(profile.component): buscando as publicacoes e diretivas no html

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicacoesService } from 'src/app/core/services/publicacoes.service';
 import { UsuariosService } from 'src/app/core/services/usuarios.service';
 import { UtilsService } from 'src/app/core/services/utils.service';
-import { PerfilUsuario } from 'src/app/shared/models/perfilUsuario.model';
+import { PerfilUsuario } from 'src/app/shared/models/perfil-usuario.model';
+import { Publicacao } from 'src/app/shared/models/publicacao.model';
 
 @Component({
   selector: 'app-profile',
@@ -12,17 +14,29 @@ export class ProfileComponent implements OnInit {
 
   constructor(
     private usuariosService: UsuariosService,
-    private utils: UtilsService
+    private utils: UtilsService,
+    private publicacoesService: PublicacoesService
   ) { }
 
   usuario: PerfilUsuario;
-
+  idUsuario: number;
+  publicacoes: Publicacao[];
   ngOnInit(): void {
+    this.carregarDadosPessoais();
+    this.carregarPublicacoes();
+  }
+
+  carregarDadosPessoais() {
     const usuario = this.utils.usuarioAtual();
-    this.usuariosService.perfil(usuario.id_usuario).subscribe(data => {
+    this.idUsuario = usuario.id_usuario;
+    this.usuariosService.perfil(this.idUsuario).subscribe(data => {
       this.usuario = data[0];
       console.log(this.usuario);
     });
+  }
+
+  carregarPublicacoes() {
+    this.publicacoesService.publicacoesUsuario(this.idUsuario).subscribe(data => this.publicacoes = data);
   }
 
 }
