d856ce38b232f16f4d2c2704931b31bcb7cbbd92
Pedro <pedrotzf@gmail.com>
2021-06-08 18:59:36 -0300

    feat(publication.component): passando Input e inserindo dados da classe
    de publicacao

--- src/app/modules/publication/publication.component.ts
+++ src/app/modules/publication/publication.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Publicacao } from 'src/app/shared/models/publicacao.model';
 
 @Component({
   selector: 'app-publication',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publication.component.css']
 })
 export class PublicationComponent implements OnInit {
+  @Input() publicacao: Publicacao;
 
   constructor() { }
 
